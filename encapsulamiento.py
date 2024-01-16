34ab9efe55fb311a9bb7e0e38eabbb064ec3a6a1
Eduardo <eduardoramirez0104@gmail.com>
2024-01-15 20:34:07 -0400

    "simplificación del codigo"

--- encapsulamiento.py
+++ encapsulamiento.py
@@ -1,72 +1,34 @@
-import pygame
-import sys
-
-pygame.init()
-
-# Tamaño de la ventana
-WIDTH, HEIGHT = 800, 600
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Personajes que se Mueven")
-clock = pygame.time.Clock()
-# Clase para el personaje
-
-
 class Character:
-    def __init__(self, x, y, width, height, color, speed, move_keys):
-        self.x = x
-        self.y = y
+    def __init__(self, width, height, color, speed):
         self.width = width
         self.height = height
         self.color = color
         self.speed = speed
-        self.move_keys = move_keys
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-
-    def move(self, keys):
-        if keys[self.move_keys[0]]:
-            self.x -= self.speed
-        if keys[self.move_keys[1]]:
-            self.x += self.speed
-        if keys[self.move_keys[2]]:
-            self.y -= self.speed
-        if keys[self.move_keys[3]]:
-            self.y += self.speed
 
 
 # Crear los personajes
 player1 = Character(
     50,
     50,
-    50,
-    50,
     (255, 0, 0),
-    1,
-    [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN],
-)
-player2 = Character(
-    200, 200, 50, 50, (0, 0, 255), 2, [pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s]
+    2,
 )
 
-# Bucle principal
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+print(player1.speed)
 
-    keys = pygame.key.get_pressed()
 
-    player1.move(keys)
-    player2.move(keys)
+class Ground:
+    def __init__(self, friction):
+        self.friction = friction
 
-    win.fill((0, 0, 0))  # Limpia la pantalla
+    def applyFriction(self, character):
+        character.speed *= self.friction
 
-    player1.draw(win)
-    player2.draw(win)
-    pygame.display.update()
-    clock.tick(60)
 
-pygame.quit()
-sys.exit()
+# Crear la instancia de la clase Ground
+ground = Ground(friction=0.9)
+
+# Aplicar la fricción a los personajes
+ground.applyFriction(player1)
+
+print(player1.speed)
